f14ec27684f5ada351865f991487b8b8f0ea9064
feliciaeponou <fliciaroselinestaxy2@gmail.com>
2021-10-18 19:46:05 +0000

    Fichier sql

--- gestion_salaires.sql
+++ gestion_salaires.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : Dim 10 oct. 2021 à 15:06
+-- Généré le : lun. 18 oct. 2021 à 19:45
 -- Version du serveur :  5.7.32
 -- Version de PHP : 7.4.12
 
@@ -35,6 +35,13 @@ CREATE TABLE `demande_paiements` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Déchargement des données de la table `demande_paiements`
+--
+
+INSERT INTO `demande_paiements` (`id`, `matricule`, `periode`, `nbSeances`, `listSeances`, `volumeHoraireTotal`, `coutTotal`, `valide`, `rejete`, `paye`, `created_at`, `updated_at`) VALUES
+(3, '5951', '01/09/2021 to 14/09/2021', '10', NULL, '80', '400000', 'non', 'non', 'non', '2021-10-18 19:19:54', '2021-10-18 19:19:54');
+
 -- --------------------------------------------------------
 
 --
@@ -55,6 +62,7 @@ CREATE TABLE `employes` (
   `photo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `suspendu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -63,11 +71,12 @@ CREATE TABLE `employes` (
 -- Déchargement des données de la table `employes`
 --
 
-INSERT INTO `employes` (`id`, `matricule`, `nom_prenoms`, `date_naissance`, `genre`, `service`, `categorie`, `salaire_par_heure`, `date_debut_service`, `volume_horaire`, `photo`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, '0123', 'Oulai Bernard', '27/03/1980', 'H', 'Recrutement', 'RH', '5000', '16/08/2010', '0', 'photophoto', 'oulaibernard@entreprise.com', 'oulaibernard', '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(2, '1011', 'Koffi Amenan', '16/06/1992', 'F', 'Marketing', 'Marketing et communication', '3000', '08/01/2016', '0', 'photophoto', 'amenankoffi@entreprise.com', 'amenankoffi', '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(3, '1516', 'Djirabou Leonard', '16/02/1995', 'H', 'Logistique', 'Departements achats et logistique', '8000', '01/09/2015', '0', 'photophoto', 'leonarddjirabou@entreprise.com', 'leonarddjirabou', '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(4, '9107', 'Tiemoko Bosco', '08/06/1988', 'homme', 'Service technique', 'Technicien', '5000', '04/10/2021', '0', NULL, 'tiemokobosco@gmail.com', '$2y$10$WHWvNDNNk.651XM6LpOxQeDHkE.wpAqLTVw8iRBapE/zxpzvmaHSe', '2021-10-09 18:24:07', '2021-10-09 18:24:07');
+INSERT INTO `employes` (`id`, `matricule`, `nom_prenoms`, `date_naissance`, `genre`, `service`, `categorie`, `salaire_par_heure`, `date_debut_service`, `volume_horaire`, `photo`, `email`, `password`, `suspendu`, `created_at`, `updated_at`) VALUES
+(2, '1011', 'Koffi Amenan', '16/06/1992', 'Femme', 'Marketing', 'Marketing et communication', '3000', '08/01/2016', '0', 'photophoto', 'amenankoffi@entreprise.com', 'password', 'non', '2021-10-16 03:20:33', '2021-10-16 03:20:33'),
+(3, '1516', 'Djirabou Leonard', '16/02/1995', 'Homme', 'Logistique', 'Departements achats et logistique', '8000', '01/09/2015', '0', 'photophoto', 'leonarddjirabou@entreprise.com', 'password', 'non', '2021-10-16 03:20:33', '2021-10-16 03:20:33'),
+(4, '5951', 'Bamba Madou', '01/01/1990', 'homme', 'Servicee', 'Categorie', '5000', '27/09/2021', '0', NULL, 'bambamadou@gmail.com', '$2y$10$G0M70Eb1P2wuQ03SxuI8gOjb1YFirpdeIkDq3bfcWjZ/9cD7VDeBW', 'non', '2021-10-18 17:04:06', '2021-10-18 17:04:06'),
+(5, '9157', 'Tiemoko Alphonsine', '09/02/1995', 'femme', 'Service', 'Categorie', '5000', '27/09/2021', '0', NULL, 'alphonsinetiemoko@gmail.com', '$2y$10$uR7/NvWh7J2OZU4oCqf5GOW5KVW.on9pGoihk.hrJbAivM4B0tw8a', 'non', '2021-10-18 17:11:21', '2021-10-18 17:11:21'),
+(6, '8352', 'Tiemoko Bosco', '17/05/1990', 'homme', 'Service', 'Categorie', '5000', '27/09/2021', '0', NULL, 'boscotiemoko@gmail.com', '$2y$10$MuUi5OA87rRtGScjmdUv9OHTarebrEU/teckG4ZOrpjbY6vPXWPTW', 'non', '2021-10-18 17:21:29', '2021-10-18 17:21:29');
 
 -- --------------------------------------------------------
 
@@ -102,13 +111,13 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(26, '2014_10_12_000000_create_users_table', 1),
-(27, '2014_10_12_100000_create_password_resets_table', 1),
-(28, '2019_08_19_000000_create_failed_jobs_table', 1),
-(29, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(30, '2021_09_12_091607_create_employes_table', 1),
-(31, '2021_09_17_183001_create_pointages_table', 1),
-(32, '2021_09_23_165630_create_demande_paiements_table', 1);
+(33, '2014_10_12_000000_create_users_table', 1),
+(34, '2014_10_12_100000_create_password_resets_table', 1),
+(35, '2019_08_19_000000_create_failed_jobs_table', 1),
+(36, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(37, '2021_09_12_091607_create_employes_table', 1),
+(38, '2021_09_17_183001_create_pointages_table', 1),
+(39, '2021_09_23_165630_create_demande_paiements_table', 1);
 
 -- --------------------------------------------------------
 
@@ -166,19 +175,59 @@ CREATE TABLE `pointages` (
 --
 
 INSERT INTO `pointages` (`id`, `employe_id`, `matricule`, `debutSeance`, `debutPause`, `finPause`, `finSeance`, `dateSeance`, `volumeHoraire`, `payee`, `created_at`, `updated_at`) VALUES
-(1, 1, '0123', '07:00', '12:00', '13:00', '16:00', '09/10/2021', '8', 'non', '2021-10-09 06:43:19', '2021-10-09 06:43:19'),
-(2, 2, '1011', '07:00', '12:00', '13:00', '17:00', '09/10/2021', '9', 'non', '2021-10-09 17:43:52', '2021-10-09 17:43:52'),
-(3, 4, '9107', '07:00', '12:00', '13:00', '17:00', '09/10/2021', '9', 'non', '2021-10-09 18:24:43', '2021-10-09 18:24:43'),
-(4, 1, '0123', '07:00', '12:00', '13:00', '16:00', '20/09/2021', '8', 'non', '2021-10-09 19:10:43', '2021-10-09 19:10:43'),
-(5, 1, '0123', '10:00', '12:00', '14:00', '17:00', '21/09/2021', '5', 'non', '2021-10-09 19:11:25', '2021-10-09 19:11:25'),
-(6, 1, '0123', '08:00', '12:00', '13:00', '18:00', '22/09/2021', '9', 'non', '2021-10-09 19:11:50', '2021-10-09 19:11:50'),
-(7, 1, '0123', '07:30', '12:00', '13:30', '16:00', '23/09/2021', '7', 'non', '2021-10-09 19:12:34', '2021-10-09 19:12:34'),
-(8, 1, '0123', '08:00', '12:00', '13:00', '16:00', '24/09/2021', '7', 'non', '2021-10-09 19:21:54', '2021-10-09 19:21:54'),
-(9, 1, '0123', '09:00', '13:00', '14:00', '16:00', '27/09/2021', '6', 'non', '2021-10-09 19:22:18', '2021-10-09 19:22:18'),
-(10, 1, '0123', '08:00', '12:00', '13:30', '16:00', '28/09/2021', '6.5', 'non', '2021-10-09 19:22:43', '2021-10-09 19:22:43'),
-(11, 1, '0123', '08:00', '12:00', '14:00', '17:00', '29/09/2021', '7', 'non', '2021-10-09 19:24:03', '2021-10-09 19:24:03'),
-(12, 1, '0123', '10:00', '14:00', '15:00', '17:00', '30/09/2021', '6', 'non', '2021-10-09 19:24:38', '2021-10-09 19:24:38'),
-(13, 1, '0123', '08:00', '12:00', '13:00', '17:00', '01/10/2021', '8', 'non', '2021-10-09 19:25:01', '2021-10-09 19:25:01');
+(3, 2, '1011', '07:00', '12:00', '13:00', '16:00', '04/10/2021', '8', 'non', '2021-10-17 18:52:47', '2021-10-17 18:52:47'),
+(4, 2, '1011', '07:00', '12:00', '13:00', '16:00', '05/10/2021', '8', 'non', '2021-10-17 18:57:42', '2021-10-17 18:57:42'),
+(5, 2, '1011', '07:00', '12:00', '13:00', '16:00', '06/10/2021', '8', 'non', '2021-10-17 18:58:29', '2021-10-17 18:58:29'),
+(6, 4, '5951', '08:00', '12:00', '13:00', '17:00', '01/09/2021', '8', 'oui', '2021-10-18 17:23:34', '2021-10-18 17:23:34'),
+(7, 4, '5951', '08:00', '12:00', '13:00', '17:00', '02/09/2021', '8', 'oui', '2021-10-18 17:30:38', '2021-10-18 17:30:38'),
+(8, 4, '5951', '08:00', '12:00', '13:00', '17:00', '03/09/2021', '8', 'oui', '2021-10-18 17:37:10', '2021-10-18 17:37:10'),
+(9, 4, '5951', '08:00', '12:00', '13:00', '17:00', '06/09/2021', '8', 'oui', '2021-10-18 17:37:27', '2021-10-18 17:37:27'),
+(10, 4, '5951', '08:00', '12:00', '13:00', '17:00', '07/09/2021', '8', 'oui', '2021-10-18 17:37:50', '2021-10-18 17:37:50'),
+(11, 4, '5951', '08:00', '12:00', '13:00', '17:00', '08/09/2021', '8', 'oui', '2021-10-18 17:38:15', '2021-10-18 17:38:15'),
+(12, 4, '5951', '08:00', '12:00', '13:00', '17:00', '09/09/2021', '8', 'oui', '2021-10-18 17:39:12', '2021-10-18 17:39:12'),
+(13, 4, '5951', '08:00', '12:00', '13:00', '17:00', '10/09/2021', '8', 'oui', '2021-10-18 17:39:28', '2021-10-18 17:39:28'),
+(14, 4, '5951', '08:00', '12:00', '13:00', '17:00', '13/09/2021', '8', 'oui', '2021-10-18 18:01:13', '2021-10-18 18:01:13'),
+(15, 4, '5951', '08:00', '12:00', '13:00', '17:00', '14/09/2021', '8', 'oui', '2021-10-18 18:02:23', '2021-10-18 18:02:23'),
+(16, 4, '5951', '08:00', '12:00', '13:00', '17:00', '15/09/2021', '8', 'non', '2021-10-18 18:02:45', '2021-10-18 18:02:45'),
+(17, 4, '5951', '08:00', '12:00', '13:00', '17:00', '16/09/2021', '8', 'non', '2021-10-18 18:03:03', '2021-10-18 18:03:03'),
+(18, 4, '5951', '08:00', '12:00', '13:00', '17:00', '17/09/2021', '8', 'non', '2021-10-18 18:04:22', '2021-10-18 18:04:22'),
+(19, 4, '5951', '08:00', '12:00', '13:00', '17:00', '20/09/2021', '8', 'non', '2021-10-18 18:04:39', '2021-10-18 18:04:39'),
+(20, 4, '5951', '08:00', '12:00', '13:00', '17:00', '21/09/2021', '8', 'non', '2021-10-18 18:04:55', '2021-10-18 18:04:55'),
+(21, 4, '5951', '08:00', '12:00', '13:00', '17:00', '22/09/2021', '8', 'non', '2021-10-18 18:05:12', '2021-10-18 18:05:12'),
+(22, 4, '5951', '08:00', '12:00', '13:00', '17:00', '23/09/2021', '8', 'non', '2021-10-18 18:05:31', '2021-10-18 18:05:31'),
+(23, 4, '5951', '08:00', '12:00', '13:00', '17:00', '24/09/2021', '8', 'non', '2021-10-18 18:06:14', '2021-10-18 18:06:14'),
+(24, 4, '5951', '08:00', '12:00', '13:00', '17:00', '27/09/2021', '8', 'non', '2021-10-18 18:06:40', '2021-10-18 18:06:40'),
+(25, 4, '5951', '08:00', '12:00', '13:00', '17:00', '28/09/2021', '8', 'non', '2021-10-18 18:06:57', '2021-10-18 18:06:57'),
+(26, 4, '5951', '08:00', '12:00', '13:00', '17:00', '29/09/2021', '8', 'non', '2021-10-18 18:07:14', '2021-10-18 18:07:14'),
+(27, 4, '5951', '08:00', '12:00', '13:00', '17:00', '30/09/2021', '8', 'non', '2021-10-18 18:08:09', '2021-10-18 18:08:09'),
+(28, 5, '9157', '08:00', '12:00', '13:00', '17:00', '01/09/2021', '8', 'non', '2021-10-18 18:14:23', '2021-10-18 18:14:23'),
+(29, 5, '9157', '07:00', '12:00', '13:00', '17:00', '02/09/2021', '9', 'non', '2021-10-18 18:14:38', '2021-10-18 18:14:38'),
+(30, 5, '9157', '08:00', '12:00', '13:00', '17:00', '03/09/2021', '8', 'non', '2021-10-18 18:14:54', '2021-10-18 18:14:54'),
+(31, 5, '9157', '08:00', '12:00', '13:00', '17:00', '06/09/2021', '8', 'non', '2021-10-18 18:15:16', '2021-10-18 18:15:16'),
+(32, 5, '9157', '08:00', '12:00', '13:00', '17:00', '07/09/2021', '8', 'non', '2021-10-18 18:15:45', '2021-10-18 18:15:45'),
+(33, 5, '9157', '08:00', '12:00', '13:00', '17:00', '08/09/2021', '8', 'non', '2021-10-18 18:16:15', '2021-10-18 18:16:15'),
+(34, 5, '9157', '08:00', '12:00', '13:00', '17:00', '09/09/2021', '8', 'non', '2021-10-18 18:16:32', '2021-10-18 18:16:32'),
+(35, 5, '9157', '08:00', '12:00', '13:00', '17:00', '10/09/2021', '8', 'non', '2021-10-18 18:16:47', '2021-10-18 18:16:47'),
+(36, 6, '8352', '08:00', '12:00', '13:00', '17:00', '13/09/2021', '8', 'non', '2021-10-18 18:17:54', '2021-10-18 18:17:54'),
+(37, 6, '8352', '08:00', '12:00', '13:00', '17:00', '14/09/2021', '8', 'non', '2021-10-18 18:18:11', '2021-10-18 18:18:11'),
+(38, 6, '8352', '08:00', '12:00', '13:00', '17:00', '15/09/2021', '8', 'non', '2021-10-18 18:18:27', '2021-10-18 18:18:27'),
+(39, 6, '8352', '08:00', '12:00', '13:00', '17:00', '16/09/2021', '8', 'non', '2021-10-18 18:19:02', '2021-10-18 18:19:02'),
+(40, 6, '8352', '08:00', '12:00', '13:00', '17:00', '17/09/2021', '8', 'non', '2021-10-18 18:19:21', '2021-10-18 18:19:21'),
+(41, 6, '8352', '08:00', '12:00', '13:00', '17:00', '20/09/2021', '8', 'non', '2021-10-18 18:19:37', '2021-10-18 18:19:37'),
+(42, 6, '8352', '08:00', '12:00', '13:00', '17:00', '21/09/2021', '8', 'non', '2021-10-18 18:19:55', '2021-10-18 18:19:55'),
+(43, 6, '8352', '08:00', '12:00', '13:00', '17:00', '22/09/2021', '8', 'non', '2021-10-18 18:20:11', '2021-10-18 18:20:11'),
+(44, 6, '8352', '08:00', '12:00', '13:00', '17:00', '23/09/2021', '8', 'non', '2021-10-18 18:21:04', '2021-10-18 18:21:04'),
+(45, 6, '8352', '08:00', '12:00', '13:00', '17:00', '24/09/2021', '8', 'non', '2021-10-18 18:21:21', '2021-10-18 18:21:21'),
+(46, 6, '8352', '08:00', '12:00', '13:00', '17:00', '27/09/2021', '8', 'non', '2021-10-18 18:21:38', '2021-10-18 18:21:38'),
+(47, 6, '8352', '08:00', '12:00', '13:00', '17:00', '28/09/2021', '8', 'non', '2021-10-18 18:21:58', '2021-10-18 18:21:58'),
+(48, 6, '8352', '08:00', '12:00', '13:00', '17:00', '28/09/2021', '8', 'non', '2021-10-18 18:22:33', '2021-10-18 18:22:33'),
+(49, 6, '8352', '08:00', '12:00', '13:00', '17:00', '30/09/2021', '8', 'non', '2021-10-18 18:22:59', '2021-10-18 18:22:59'),
+(50, 6, '8352', '08:00', '12:00', '13:00', '17:00', '01/10/2021', '8', 'oui', '2021-10-18 18:23:18', '2021-10-18 18:23:18'),
+(51, 6, '8352', '08:00', '12:00', '13:00', '17:00', '04/10/2021', '8', 'non', '2021-10-18 18:23:49', '2021-10-18 18:23:49'),
+(52, 6, '8352', '08:00', '12:00', '13:00', '17:00', '05/10/2021', '8', 'non', '2021-10-18 18:24:07', '2021-10-18 18:24:07'),
+(53, 6, '8352', '08:00', '12:00', '13:00', '17:00', '06/10/2021', '8', 'non', '2021-10-18 18:24:51', '2021-10-18 18:24:51'),
+(54, 6, '8352', '08:00', '12:00', '13:00', '17:00', '07/10/2021', '8', 'non', '2021-10-18 18:25:09', '2021-10-18 18:25:09'),
+(55, 6, '8352', '08:00', '12:00', '13:00', '17:00', '08/10/2021', '8', 'non', '2021-10-18 18:26:06', '2021-10-18 18:26:06');
 
 -- --------------------------------------------------------
 
@@ -204,14 +253,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `role`, `password`, `matricule`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin Admin', 'admin@gmail.com', '2021-10-09 05:54:21', 'admin', '$2y$10$LSkIZmUiPtZZ9G.yEl3hleql6.ap8vUyAjuiAbvfZjUVFgPPZeEpS', '1234', NULL, '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(2, 'Employe Employe', 'employe@gmail.com', '2021-10-09 05:54:21', 'employe', '$2y$10$Tsbs8Mz2NbU9N3cNivxa0uh1yr1a/RZjOaa//zj5FmsvtEUWol6IW', '5678', NULL, '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(3, 'Informaticien Informaticien', 'informaticien@gmail.com', '2021-10-09 05:54:21', 'informaticien', '$2y$10$TR2230MV6F6pt5aMzH.PUunxuf2oOqsbIl9hxLyco25.Xm38e0gim', '9101', NULL, '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(4, 'Directeur Directeur', 'directeur@gmail.com', '2021-10-09 05:54:21', 'directeur', '$2y$10$ntvkgmLAzzEY3C1102OwQeKioc9AmFI4tMn0.EhMzL2jBa2N/Me8q', '1213', NULL, '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(5, 'Comptable Comptable', 'comptable@gmail.com', '2021-10-09 05:54:21', 'comptable', '$2y$10$96M3Xxiygp6VyEGFGZbnwe0aW68a0hlgAoIdGCihm3AuYovZsiV.6', '1415', NULL, '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(6, 'Secretaire Comptable', 'secretaire_comptable@gmail.com', '2021-10-09 05:54:21', 'secretaire_comptable', '$2y$10$XL37xrlcF7xZRvfdcZ0DDunQzTbyUppj7Nl14.5hEOEDsyk3fAif2', '1617', NULL, '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(7, 'RH', 'rh@gmail.com', '2021-10-09 05:54:21', 'rh', '$2y$10$SFH6xFIFQeFb8j1oLU3x..17hBuFvsAwsghFXBrZylqhgxl6Fpbai', '1819', NULL, '2021-10-09 05:54:21', '2021-10-09 05:54:21'),
-(8, 'Tiemoko Bosco', 'tiemokobosco@gmail.com', NULL, 'employe', '$2y$10$b7yGUaOpyYeKOssRIszk/Oog2f2lwJArBhhexlrZZNXVV1ftNPbrC', '9107', NULL, '2021-10-09 18:24:07', '2021-10-09 18:24:07');
+(1, 'Admin Admin', 'admin@gmail.com', '2021-10-16 03:20:32', 'admin', '$2y$10$PJ3se9pCr4T2BmcKdBicgOaX5WzkabtXyQ0NtAr68FTW1phjXRD/C', '1234', NULL, '2021-10-16 03:20:32', '2021-10-16 03:20:32'),
+(2, 'Employe Employe', 'employe@gmail.com', '2021-10-16 03:20:32', 'employe', '$2y$10$fMrXbT.cHnqRcsRZhgzcW.idkEUooG1Es5uT2CAoGgv/YiCpWqI.K', '5678', NULL, '2021-10-16 03:20:32', '2021-10-16 03:20:32'),
+(3, 'Informaticien Informaticien', 'informaticien@gmail.com', '2021-10-16 03:20:32', 'informaticien', '$2y$10$4I.g32XcP7pf7a0MevXHnOpVOQzGIlEQcYNdnFA0siCsjnNy5tOhq', '9101', NULL, '2021-10-16 03:20:32', '2021-10-16 03:20:32'),
+(4, 'Directeur Directeur', 'directeur@gmail.com', '2021-10-16 03:20:32', 'directeur', '$2y$10$8N4xPyPIKooEXGDhj2OAy.i8.Ts63lKv3XPLgC2vOZswNNVuj97au', '1213', NULL, '2021-10-16 03:20:32', '2021-10-16 03:20:32'),
+(5, 'Comptable Comptable', 'comptable@gmail.com', '2021-10-16 03:20:32', 'comptable', '$2y$10$oPQGRlEqdDj/tEtb0vSqVeD7aixWvYKtUxinDFBG8Ck.kv/Nb49DS', '1415', NULL, '2021-10-16 03:20:32', '2021-10-16 03:20:32'),
+(6, 'Secretaire Comptable', 'secretaire_comptable@gmail.com', '2021-10-16 03:20:32', 'secretaire_comptable', '$2y$10$3Vza9Y.qU65V1uwZ0Lgzpe33DzrdlkzXpffnC.zWZh90CE3uOjJBq', '1617', NULL, '2021-10-16 03:20:32', '2021-10-16 03:20:32'),
+(7, 'RH', 'rh@gmail.com', '2021-10-16 03:20:32', 'rh', '$2y$10$jye/phQ3gn7IrIH8S.OPau6r1Mkku0Pk/ue.8haZimCGZTq0MvdV6', '1819', NULL, '2021-10-16 03:20:32', '2021-10-16 03:20:32'),
+(8, 'Oulai Bernard', 'oulaibernard@gmail.com', '2021-10-16 03:20:32', 'employe', '$2y$10$Wm6Co3FsaM8SaQapT7Pxgu9H4lBOTnnkjrB6l7grBv2hIMmPGaTcm', '0123', NULL, '2021-10-16 03:20:32', '2021-10-16 03:20:32'),
+(9, 'Koffi Amenan', 'amenankoffi@gmail.com', '2021-10-16 03:20:32', 'employe', '$2y$10$uUlkyqM/HuVUJlgB9mAEWulMUgb8KQjg4s1R6Yavhw1Iew.061hhC', '1011', NULL, '2021-10-16 03:20:33', '2021-10-16 03:20:33'),
+(10, 'Djirabou Leonard', 'leonarddjirabou@gmail.com', '2021-10-16 03:20:33', 'employe', '$2y$10$Z5AssNr.EAcSd7I6IcX2tuJpLhwQSK9p0dnq88ASieavOipR.0pBS', '1516', NULL, '2021-10-16 03:20:33', '2021-10-16 03:20:33'),
+(11, 'Bamba Madou', 'bambamadou@gmail.com', NULL, 'employe', '$2y$10$fVFCu.Dg0VceiOmrMPmHsuFjoaLRZEULCoEzylKIZrS5iss.lbfiO', '5951', NULL, '2021-10-18 17:04:06', '2021-10-18 17:04:06'),
+(12, 'Tiemoko Alphonsine', 'alphonsinetiemoko@gmail.com', NULL, 'employe', '$2y$10$qJHzRtsSqtwTC7grt8cmbembXiG0Z4kkzlYAlk9yzkGM1LKTJp8Va', '9157', NULL, '2021-10-18 17:11:21', '2021-10-18 17:11:21'),
+(13, 'Tiemoko Bosco', 'boscotiemoko@gmail.com', NULL, 'employe', '$2y$10$aX1h9pDLTfjqiYawO1.4QOW.xaFsXCuPrm8OXi95ziBlwfd15Yh/W', '8352', NULL, '2021-10-18 17:21:29', '2021-10-18 17:21:29');
 
 --
 -- Index pour les tables déchargées
@@ -280,13 +334,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `demande_paiements`
 --
 ALTER TABLE `demande_paiements`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `employes`
 --
 ALTER TABLE `employes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `failed_jobs`
@@ -298,7 +352,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT pour la table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT pour la table `personal_access_tokens`
@@ -310,13 +364,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT pour la table `pointages`
 --
 ALTER TABLE `pointages`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Contraintes pour les tables déchargées
